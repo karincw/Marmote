cf48b7121952c739cb502d38233a12e74613f950
Karin <karin.cw07@gmail.com>
2025-05-20 17:07:04 +0900

    TItle
    # Conflicts:
    #	Siho/Assets/01. Scripts/Karin/Core/DataLinkManager.cs
    #	Siho/Assets/05. Fonts/BMJUA_ttf SDF.asset
    #	Siho/Assets/05. Fonts/Hakgyoansim Dunggeunmiso TTF B SDF.asset
    #	Siho/Assets/05. Fonts/Name Font Blur.asset
    #	Siho/ProjectSettings/EditorBuildSettings.asset

--- Siho/Assets/01. Scripts/Karin/UI/InventoryPanel.cs
+++ Siho/Assets/01. Scripts/Karin/UI/InventoryPanel.cs
@@ -17,7 +17,7 @@ namespace karin.ui
             _canvasGroup = GetComponent<CanvasGroup>();
             _rectTransform = transform as RectTransform;
             originPos = _rectTransform.localPosition;
-            Close();
+            _rectTransform.localPosition = originPos - new Vector2(-1000, 0);
         }
 
         public void Open()
